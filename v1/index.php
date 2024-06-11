bf0690c4ac531aba75c73423cb2f85d942597ceb
SrEmanuelV <emanuelvillalobos233@gmail.com>
2024-06-11 14:50:45 -0500

    Add files via upload

---
+++ v1/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio | PortaFolio</title>
+    <link rel="stylesheet" href="css/main.css">
+    <script src="https://kit.fontawesome.com/e85d0a813a.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!-- HEADER -->
+    <header>
+        <div class="title">
+            <h1>PortaFolio</h1>
+        </div>
+
+        <div class="menu">
+            <a href="index.php">Inicio</a>
+            <a href="#">¿Quien Soy?</a>
+            <a href="#">Habilidades</a>
+        </div>
+
+        <div class="contact">
+            <a href="#">Contacto</a>
+        </div>
+    </header>
+    
+
+    <script src="js/main.js"></script>
+</body>
+</html>
